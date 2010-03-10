dadaa08e099bb563956886e4387619a8dc87dc9a
Sergey Varaksin <varaksin86@gmail.com>
2010-03-10 16:55:35 +0600

    fixed group_by for postgresql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,8 @@ module ApplicationHelper
   
   # outputs tag cloud for posts
   def tag_cloud_for_posts(classes)
-    tags = Tag.all(:joins => :taggings, :select => "#{Tag.table_name}.*, COUNT(*) AS count", :group => :tag_id)
+    tags = Tag.all(:select => "*, (select COUNT(tag_id) from #{Tagging.table_name} where tag_id=#{Tag.table_name}.id) AS count")
+    # tags = Tag.all(:joins => :taggings, :select => "#{Tag.table_name}.*, COUNT(*) AS count", :group => :tag_id)
     return [] if tags.empty?
     max_count = tags.sort_by(&:count).last.count.to_f
     tags.each do |tag|
